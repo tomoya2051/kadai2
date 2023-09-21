e8c382263b0b7dcaaba293640b714474dd35dd2f
tomoya2051 <123750776+tomoya2051@users.noreply.github.com>
2023-09-21 05:39:50 +0000

    [Finish] 3-15

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def new
+      @book = Book.new
+  end
+
+  def index
+      @books = Book.all
+      @book = Book.new
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def create
+      @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = "successfully"
+       redirect_to book_path(@book.id)
+    else flash[:notice] ="error"
+      @books = Book.all
+      render:index
+    end
+  end
+
+  def update
+      @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] ="successfully"
+       redirect_to book_path(@book.id)
+    else flash[:notice] ="error"
+      @books = Book.all
+      render:edit
+    end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+    if book.destroy
+       redirect_to '/books'
+       flash[:notice] ="successfully"
+    else flash[:notice] ="error"
+      @books = Book.all
+       render:index
+    end
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
